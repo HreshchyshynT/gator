9edd3fa744fbeacf43e25af5b536cf5e764fa496
HreshchyshynT <hreshchyshyn.t@gmail.com>
2025-12-15 23:24:05 +0200

    add sqlc config, add query to create and get user, open db connection and use it during login and register commands

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
 	cfg "github.com/hreshchyshynt/gator/internal/config"
+	"github.com/hreshchyshynt/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -15,10 +21,16 @@ func main() {
 		log.Fatalf("error read config: %v", err)
 	}
 
-	state := NewState(config)
+	db, err := sql.Open("postgres", config.DbUrl)
+	if err != nil {
+		log.Fatalln("Can't open database connection")
+	}
+
+	state := NewState(config, database.New(db))
 	commands := NewCommands()
 
 	commands.Register("login", handleLogin)
+	commands.Register("register", handleRegister)
 
 	args := os.Args
 
@@ -40,7 +52,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("error read new config: %v", err)
 	}
-	fmt.Printf("New config read: %v", config)
 }
 
 func handleLogin(s *State, cmd Command) error {
@@ -50,11 +61,46 @@ func handleLogin(s *State, cmd Command) error {
 
 	username := cmd.args[0]
 
-	if err := s.config.SetUser(username); err != nil {
+	user, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return err
+	}
+
+	if err := s.config.SetUser(user.Name); err != nil {
 		return err
 	}
 
 	fmt.Printf("User has been set: %v\n", username)
+
+	return nil
+}
+
+func handleRegister(s *State, cmd Command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("Register command requires username argument")
+	}
+
+	username := cmd.args[0]
+
+	now := time.Now()
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      username,
+	}
+
+	user, err := s.db.CreateUser(context.Background(), params)
+
+	if err != nil {
+		return err
+	}
+
+	if err := s.config.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Printf("User has been created: %v\n", user)
 
 	return nil
 }
--- state.go
+++ state.go
@@ -1,13 +1,21 @@
 package main
 
-import cfg "github.com/hreshchyshynt/gator/internal/config"
+import (
+	cfg "github.com/hreshchyshynt/gator/internal/config"
+	"github.com/hreshchyshynt/gator/internal/database"
+)
 
 type State struct {
+	db     *database.Queries
 	config *cfg.Config
 }
 
-func NewState(config *cfg.Config) State {
+func NewState(
+	config *cfg.Config,
+	db *database.Queries,
+) State {
 	return State{
 		config: config,
+		db:     db,
 	}
 }
