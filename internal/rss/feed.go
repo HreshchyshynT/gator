72bc4f8940f40c53c508e40de388210e82cca2d8
HreshchyshynT <hreshchyshyn.t@gmail.com>
2026-01-21 14:10:05 +0200

    add custom RSSItem parsing to avoid link shadowing by namespaced link elements

--- internal/rss/feed.go
+++ internal/rss/feed.go
@@ -1,6 +1,10 @@
 package rss
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"strings"
+)
 
 type RSSFeed struct {
 	Channel struct {
@@ -16,6 +20,74 @@ type RSSItem struct {
 	Link        string `xml:"link"`
 	Description string `xml:"description"`
 	PubDate     string `xml:"pubDate"`
+}
+
+func (i *RSSItem) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
+	for {
+		tok, err := dec.Token()
+		if err != nil {
+			return err
+		}
+
+		switch t := tok.(type) {
+		case xml.StartElement:
+			switch t.Name.Local {
+			case "title":
+				var v string
+				if err := dec.DecodeElement(&v, &t); err != nil {
+					return err
+				}
+				i.Title = strings.TrimSpace(v)
+
+			case "link":
+				// Distinguish by namespace:
+				if t.Name.Space == "" {
+					// RSS2 <link>text</link>
+					var v string
+					if err := dec.DecodeElement(&v, &t); err != nil {
+						return err
+					}
+					v = strings.TrimSpace(v)
+					// Keep the RSS text link (don’t overwrite with empty)
+					if v != "" {
+						i.Link = v
+					}
+				} else {
+					// some other namespace: just consume it
+					var skip any
+					if err := dec.DecodeElement(&skip, &t); err != nil {
+						return err
+					}
+				}
+
+			case "description":
+				var v string
+				if err := dec.DecodeElement(&v, &t); err != nil {
+					return err
+				}
+				i.Description = strings.TrimSpace(v)
+
+			case "pubDate":
+				var v string
+				if err := dec.DecodeElement(&v, &t); err != nil {
+					return err
+				}
+				i.PubDate = strings.TrimSpace(v)
+
+			default:
+				// Consume any element we don't care about (guid, category, media:content, dc:creator, etc.)
+				var skip any
+				if err := dec.DecodeElement(&skip, &t); err != nil {
+					return err
+				}
+			}
+
+		case xml.EndElement:
+			if t.Name.Local == start.Name.Local && t.Name.Space == start.Name.Space {
+				return nil
+			}
+		}
+	}
 }
 
 func (ri RSSItem) String() string {
